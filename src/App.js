2b36de065d6b9de606b5f85df973bf5f46330f8a
zhao Seo <tkddnjs5652@naver.com>
2020-02-11 11:23:37 +0900

    create customer components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customer = {
+  'name' : '서상원',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
-      </div>
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
     );
   }
 }
